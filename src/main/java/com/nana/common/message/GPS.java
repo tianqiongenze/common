0e469a2e9466a5907cc8a071e42b39759712b497
Zhihui Li <zhihui.li@intel.com>
2015-06-19 12:46:56 +0800

    rename ChatMessage

---
+++ src/main/java/com/nana/common/message/GPS.java
@@ -0,0 +1,36 @@
+package com.nana.common.message;
+
+/**
+ * GPS info
+ *
+ */
+public class GPS {
+	private double longitude;
+	private double latitude;
+	private double height;
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+}
